01a2604e2cefdd75f2571dcdb2c431af60676a3a
Garik Kalashyan <Garik_Kalashyan@epam.com>
2018-10-19 13:07:41 +0400

    is added procfile to sources

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
-app.listen(process.env.PORT || 4200);
+app.listen(process.env.PORT || 8080);
 
 const path = require('path');
 
